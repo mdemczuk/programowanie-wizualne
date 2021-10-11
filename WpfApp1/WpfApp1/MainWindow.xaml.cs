4cacee62096938a47089b722f5ffcbc68aeecb76
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-10-11 22:25:37 +0200

    Add files via upload

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,17 +20,25 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer dispTimer = new System.Windows.Threading.DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
+            dispTimer.Tick += Current_Time;
+            dispTimer.Interval = TimeSpan.FromMilliseconds(1);
+            dispTimer.Tick += Current_Time;
+            dispTimer.Start();
         }
 
-        private void STOPER_Click(object sender, RoutedEventArgs e)
+        private void Current_Time(object sender, EventArgs e)
         {
-            public Window1()
-            {
-                InitializeComponent();
-            }
+            timer.Content = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 stopwatchWindow = new Window1();
+            stopwatchWindow.Show();
         }
     }
 }
--- WpfApp1/WpfApp1/Window1.xaml.cs
+++ WpfApp1/WpfApp1/Window1.xaml.cs
@@ -13,13 +13,32 @@ using System.Windows.Shapes;
 namespace WpfApp1
 {
     /// <summary>
-    /// Logika interakcji dla klasy Window1.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
     public partial class Window1 : Window
     {
+        System.Windows.Threading.DispatcherTimer dispTimer = new System.Windows.Threading.DispatcherTimer();
+        DateTime date_time = new DateTime();
         public Window1()
         {
             InitializeComponent();
+            dispTimer.Tick += Current_Time;
+            dispTimer.Interval = TimeSpan.FromMilliseconds(1);
+        }
+        private void Current_Time(object sender, EventArgs e)
+        {
+            date_time = date_time.AddSeconds(1);
+            stopwatch.Content = date_time.ToString("HH:mm:ss");
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            dispTimer.Start();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            dispTimer.Stop();
         }
     }
 }
