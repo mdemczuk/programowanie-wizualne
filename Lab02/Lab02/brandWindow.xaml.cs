42f22f9a9336428fcc5d616a09d8ed40b9b03b69
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-10-18 16:36:42 +0200

    Add files via upload

---
+++ Lab02/Lab02/brandWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab02
+{
+    /// <summary>
+    /// Interaction logic for brandWindow.xaml
+    /// </summary>
+    public partial class brandWindow : Window
+    {
+        public brandWindow(MainWindow mainWindow)
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void brandReturn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Lab02/Lab02/engineWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab02
+{
+    /// <summary>
+    /// Interaction logic for engineWindow.xaml
+    /// </summary>
+    public partial class engineWindow : Window
+    {
+        public engineWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void brandReturn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void enginePower_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
