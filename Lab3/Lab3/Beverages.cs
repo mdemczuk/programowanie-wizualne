226c52741b880679a2a62a1bc45cb547251f3e31
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-10-25 16:57:46 +0200

    Add files via upload

---
+++ Lab3/Lab3/Beverages.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class Beverages
+    {
+        private string itemName;
+        private int itemCount;
+        private int itemIndex = 0;
+        public string ItemName 
+        {
+            get { return itemName; } 
+            set { itemName = value; } 
+        }
+
+        public int ItemCount
+        {
+            get { return itemCount; }
+            set { itemCount = value; }
+        }
+
+        public int ItemIndex
+        {
+            get { return itemIndex; }
+            set { itemIndex = value; }
+        }
+
+        public keeper(string ItemName, int ItemIndex, int ItemCount)
+        {
+            this.ItemName = ItemName;
+            this.ItemIndex = ItemIndex;
+            this.ItemCount = ItemCount;
+        }
+
+        public void SetItemName(string itName)
+        {
+            ItemName = itName;
+        }
+
+        public void SetItemCount(int itCount)
+        {
+            ItemCount = itCount;
+        }
+
+        public void SetItemIndex()
+        {
+            ItemIndex += 1;
+        }
+    }
+}
+
+    
---
+++ Lab3/Lab3/WindowAdd.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab3
+{
+    /// <summary>
+    /// Interaction logic for WindowAdd.xaml
+    /// </summary>
+    public partial class WindowAdd : Window
+    {
+        public string itName;
+        public int itCount;
+        MainWindow List;
+
+        public WindowAdd(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            List = mainWindow;
+        }
+
+        private void NameTextChanged(object sender, TextChangedEventArgs e)
+        {
+           itName = textbox_name.Text;
+        }
+
+        private void CountTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!int.TryParse(textbox_count.Text, out itCount))
+            {
+                itCount = 0;
+            }
+        }
+
+        private void button_add_item_Click(object sender, RoutedEventArgs e)
+        {
+            Beverages AddBeverage = new Beverages();
+            AddBeverage.ItemName = itName;
+            AddBeverage.ItemCount = itCount;
+            AddBeverage.ItemIndex += 1;
+            List.listView.Items.Add(AddBeverage);
+            this.Close();
+        }
+    }
+}
