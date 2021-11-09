538df1f9dfe2df3442163efe6bc6dc047cb59cea
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-11-09 20:20:46 +0100

    Add files via upload

--- Lab04/Lab04/MainWindow.xaml.cs
+++ Lab04/Lab04/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,7 @@ namespace Lab04
         }
 
         public int ScaleXVal = -1;
+        public int RotateValue = 0;
         private void upload_button_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
@@ -59,11 +61,67 @@ namespace Lab04
             imgPhoto.RenderTransform = flipTrans;
         }
 
-        private void optionB_Click(object sender, RoutedEventArgs e)
+       private void optionB_Click(object sender, RoutedEventArgs e)
         {
-            TransformedBitmap transformBmp = new TransformedBitmap();
-            transformBmp.BeginInit();
+            RotateValue += 90;
+            RotateTransform rotateTransform = new RotateTransform(RotateValue);
+            imgPhoto.RenderTransform = rotateTransform;
 
+        }
+
+        private void optionC_Click(object sender, RoutedEventArgs e)
+        {
+            var dObject = new DataObject(DataFormats.Bitmap, imgPhoto.Source, true);
+            var bitmap = dObject.GetData("System.Drawing.Bitmap") as System.Drawing.Bitmap;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    System.Drawing.Color getColor = bitmap.GetPixel(x, y);
+                    if((getColor.R < 50) && (getColor.G < 150) && (getColor.B < 50))
+                    {
+                        System.Drawing.Color whiteColor = System.Drawing.Color.White;
+                        bitmap.SetPixel(x, y, whiteColor);
+                    } 
+                }
+            }
+
+            MemoryStream mStream = new MemoryStream();
+            ((System.Drawing.Bitmap)bitmap).Save(mStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage bImage = new BitmapImage();
+            bImage.BeginInit();
+            mStream.Seek(0, SeekOrigin.Begin);
+            bImage.StreamSource = mStream;
+            bImage.EndInit();
+
+            imgPhoto.Source = bImage;
+        }
+
+        private void optionD_Click(object sender, RoutedEventArgs e)
+        {
+            var dObject = new DataObject(DataFormats.Bitmap, imgPhoto.Source, true);
+            var bitmap = dObject.GetData("System.Drawing.Bitmap") as System.Drawing.Bitmap;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    System.Drawing.Color neg = bitmap.GetPixel(x, y);
+                    neg = System.Drawing.Color.FromArgb(255, (255 - neg.R), (255 - neg.G), (255 - neg.B));
+                    bitmap.SetPixel(x, y, neg);
+                }
+            }
+
+            MemoryStream mStream = new MemoryStream();
+            ((System.Drawing.Bitmap)bitmap).Save(mStream, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage bImage = new BitmapImage();
+            bImage.BeginInit();
+            mStream.Seek(0, SeekOrigin.Begin);
+            bImage.StreamSource = mStream;
+            bImage.EndInit();
+
+            imgPhoto.Source = bImage;
         }
     }
 }
