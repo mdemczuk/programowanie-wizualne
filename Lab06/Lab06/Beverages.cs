347c80ee9a50fcecf824f56ce4c0a996101ccb99
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-11-23 17:47:06 +0100

    Add files via upload

---
+++ Lab06/Lab06/Beverages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06
+{
+    public class Beverages
+    {
+        private string itemName;
+        private int itemCount;
+        private int itemIndex = InxNumber.IndexNumber;
+        public string ItemName
+        {
+            get { return itemName; }
+            set { itemName = value; }
+        }
+
+        public int ItemCount
+        {
+            get { return itemCount; }
+            set { itemCount = value; }
+        }
+
+        public int ItemIndex
+        {
+            get { return itemIndex; }
+            set { itemIndex = value; }
+        }
+
+    }
+}
+
+
---
+++ Lab06/Lab06/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+using MessageBox = System.Windows.Forms.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+
+namespace Lab06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    static class InxNumber
+    {
+        public static int IndexNumber = 0;
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_add_Click(object sender, RoutedEventArgs e)
+        {
+            WindowAdd aWindow = new WindowAdd(this);
+            aWindow.Show();
+        }
+
+        private void button_save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() ?? true)
+            {
+                using (FileStream fileWriter = new FileStream(dialog.FileName, FileMode.OpenOrCreate))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(Beverages[]));
+                    serializer.Serialize(fileWriter, listView.Items.OfType<Beverages>().ToList().ToArray());
+                }
+            }
+        }
+
+        private void button_read_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() ?? true)
+            {
+                using (FileStream fileReader = new FileStream(dialog.FileName, FileMode.OpenOrCreate))
+                {
+                    InxNumber.IndexNumber = 1;
+                    listView.Items.Clear();
+                    XmlSerializer deserializer = new XmlSerializer(typeof(List<Beverages>));
+                    foreach (Beverages item in (List<Beverages>)deserializer.Deserialize(fileReader))
+                    {
+                        listView.Items.Add(item);
+                        InxNumber.IndexNumber++;
+                    }
+                    listView.Items.Refresh();
+                }
+                listView.Items.Refresh();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you want to save your data?", "Save data", MessageBoxButtons.YesNo);
+            switch (dialogResult)
+            {
+                case System.Windows.Forms.DialogResult.Yes:
+                    SaveFileDialog dialog = new SaveFileDialog();
+                    if (dialog.ShowDialog() ?? true)
+                    {
+                        using (FileStream fileWriter = new FileStream(dialog.FileName, FileMode.OpenOrCreate))
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(List<Beverages>));
+                            serializer.Serialize(fileWriter, listView.Items.OfType<List<Beverages>>().ToList().ToArray());
+                        }
+                    }
+                    break;
+                case System.Windows.Forms.DialogResult.No:
+                    break;
+            }
+        }
+    }
+}
