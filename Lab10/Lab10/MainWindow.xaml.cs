ee17bc7c7faf62914fd130af9fd548f8e2ea37e2
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2021-12-24 23:41:15 +0100

    Add files via upload

---
+++ Lab10/Lab10/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Lab10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    class Song
+    {
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
+        public MainWindow()
+        {
+            string projectDir = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName;
+            string songsDir = System.IO.Path.Combine(projectDir, "songs");
+
+            InitializeComponent();
+            ListBox_Songs.Items.Add(new Song { Name = "Song 1", FilePath = System.IO.Path.Combine(songsDir, "song1.mp3") });
+            ListBox_Songs.Items.Add(new Song { Name = "Song 2", FilePath = System.IO.Path.Combine(songsDir, "song2.mp3") });
+            ListBox_Songs.Items.Add(new Song { Name = "Song 3", FilePath = System.IO.Path.Combine(songsDir, "song3.mp3") });
+            ListBox_Songs.Items.Add(new Song { Name = "The Best Song", FilePath = System.IO.Path.Combine(songsDir, "bestsong.mp3") });
+
+            ListBox_Songs.Items.Refresh();
+        }
+
+        private void Button_Play_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Open(new Uri(((Song)ListBox_Songs.SelectedItem).FilePath));
+            mediaPlayer.Play();
+        }
+
+        private void Button_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            mediaPlayer.Stop();
+        }
+    }
+}
