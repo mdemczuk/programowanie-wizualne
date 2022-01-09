6ceb97e4a1bf4df25d98e4157e00b50b560aea65
mdemczuk <61699785+mdemczuk@users.noreply.github.com>
2022-01-09 12:45:16 +0100

    Add files via upload

---
+++ Lab11/Lab11/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+
+namespace Lab11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Decode_Click(object sender, RoutedEventArgs e)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(TextBox_Key.Text));
+                aes.IV = Enumerable.Repeat((byte)0x0, 16).ToArray();
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                        {
+                            streamWriter.Write(TextBox_Text.Text);
+                        }
+                        TextBox_Hash.Text = Convert.ToBase64String(memoryStream.ToArray());
+                    }
+                }
+            }
+        }
+
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() ?? true)
+            {
+                File.WriteAllText(dialog.FileName, TextBox_Hash.Text);
+            }
+        }
+    }
+}
